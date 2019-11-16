53352c2c72c981a1ca41c59b5ea560d26a7c127e
yariki <iyariki.ya@gmail.com>
2019-11-16 22:29:52 +0200

    fixed logging

--- src/TaskWorkspace/Infrastructure/WorkspaceLogger.cs
+++ src/TaskWorkspace/Infrastructure/WorkspaceLogger.cs
@@ -1,17 +1,33 @@
 ﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.Debugger.Clr.Cpp;
 using NLog;
+using NLog.Config;
+using NLog.Targets;
 
 namespace TaskWorkspace.Infrastructure
 {
     public class WorkspaceLogger
     {
         private static readonly Lazy<WorkspaceLogger> _logger = new Lazy<WorkspaceLogger>(() => new WorkspaceLogger());
-
         private WorkspaceLogger()
         {
+
+            var config = new LoggingConfiguration();
+            var fileTarget = new FileTarget()
+            {
+                Name = "File",
+                FileName = "${specialfolder:folder=LocalApplicationData}/TaskWorkspace/logs/${shortdate}.log",
+                Layout = "${longdate} ${logger} ${uppercase:${level}} ${message}${exception:format=ToString}",
+            };
+            config.AddTarget(fileTarget);
+            config.AddRuleForAllLevels(fileTarget);
+            LogManager.Configuration = config;
+            Logger = LogManager.GetLogger("TaskWorkspace");
+
         }
 
-        private static Logger Logger => LogManager.GetCurrentClassLogger();
+        private static Logger Logger;
 
         public static WorkspaceLogger Log => _logger.Value;
 
--- src/TaskWorkspace/TaskWorkspacePackage.cs
+++ src/TaskWorkspace/TaskWorkspacePackage.cs
@@ -52,6 +52,7 @@ namespace TaskWorkspace
         protected override async Task InitializeAsync(CancellationToken cancellationToken,
             IProgress<ServiceProgressData> progress)
         {
+            WorkspaceLogger.Log.Info("The Workspace Manager is loading...");
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
