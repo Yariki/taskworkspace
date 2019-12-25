26206db571744490a040308f09ec0ec669b520b1
yariki <iyariki.ya@gmail.com>
2019-12-25 15:06:05 +0200

    added rest code for uploading and downloading backups

--- src/TaskWorkspace/Backup/BackupHelpers/GoogleHelper.cs
+++ src/TaskWorkspace/Backup/BackupHelpers/GoogleHelper.cs
@@ -1,15 +1,21 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.IO.IsolatedStorage;
+using System.Linq;
 using System.Runtime.Serialization.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using Google.Apis.Auth.OAuth2;
+using Google.Apis.Download;
 using Google.Apis.Drive.v3;
 using Google.Apis.Services;
 using Google.Apis.Util.Store;
 using Microsoft.VisualStudio.Debugger.Clr.Cpp;
 using TaskWorkspace.Backup.Interfaces;
+using TaskWorkspace.Infrastructure;
 using TaskWorkspace.Model;
+using File = Google.Apis.Drive.v3.Data.File;
 
 namespace TaskWorkspace.Backup.BackupHelpers
 {
@@ -34,14 +40,88 @@ namespace TaskWorkspace.Backup.BackupHelpers
 			_filename = filename;
 		}
 
-		public Task<bool> UploadBackup()
+		public async Task<bool> UploadBackup()
 		{
-			throw new System.NotImplementedException();
+			try
+			{
+				var service = GetDriveService();
+				var parentFolderId = GetDefaultFolderId() ?? CreateAndGetDefaultFolderId(service);
+
+				//remove
+				var removeLIst = retrieveAllFiles(service,$"name = '{_filename}'");
+				foreach(var fileRemove in removeLIst)
+				{
+					await service.Files.Delete(fileRemove.Id).ExecuteAsync();
+				}
+
+				// upload
+				var fileMetadata = new File()
+				{
+					Name = _filename,
+					Parents = new List<string>() { parentFolderId }
+				};
+				FilesResource.CreateMediaUpload request;
+				using(var stream = new System.IO.FileStream(_fullFileName,
+										System.IO.FileMode.Open))
+				{
+					request = service.Files.Create(
+						fileMetadata,stream,DefaultMimeType);
+					request.Fields = "id";
+
+					await request.UploadAsync();
+				}
+
+				return true;
+			}
+			catch (Exception e)
+			{
+				WorkspaceLogger.Log.Error(e);
+				return false;
+			}
 		}
 
-		public Task<bool> DownloadBackup()
+		public async Task<bool> DownloadBackup()
 		{
-			throw new System.NotImplementedException();
+			try
+			{
+				var service = GetDriveService();
+				var fileList = retrieveAllFiles(service,$"name = '{_filename}'");
+				if(!fileList.Any())
+				{
+					WorkspaceLogger.Log.Warn($"The '{_filename}' backup was not found.");
+					return false;
+				}
+
+				var fileResp = service.Files.Get(fileList.First().Id);
+				//download
+				var memStream = new MemoryStream();
+				fileResp.MediaDownloader.ProgressChanged += progress =>
+				{
+					switch(progress.Status)
+					{
+						case DownloadStatus.Completed:
+							using(var fileStream = System.IO.File.Create(_fullFileName))
+							{
+								memStream.WriteTo(fileStream);
+								fileStream.Flush();
+							}
+							break;
+						case DownloadStatus.Failed:
+							if(progress.Exception != null)
+								Console.WriteLine(progress.Exception.Message);
+							break;
+
+					}
+				};
+				await fileResp.DownloadAsync(memStream);
+
+				return true;
+			}
+			catch (Exception e)
+			{
+				WorkspaceLogger.Log.Error(e);
+				return false;
+			}
 		}
 
 
@@ -49,11 +129,11 @@ namespace TaskWorkspace.Backup.BackupHelpers
 		private OAuthInfo GetAuthInfo ()
 		{
 			var oathfile = Path.Combine(Path.GetDirectoryName(typeof(DropboxHelper).Assembly.Location), GoogleOAuthInfo);
-			if(!File.Exists(oathfile))
+			if(!System.IO.File.Exists(oathfile))
 			{
 				return null;
 			}
-			var stream = File.Open(oathfile, FileMode.Open, FileAccess.Read, FileShare.Read);
+			var stream = System.IO.File.Open(oathfile, FileMode.Open, FileAccess.Read, FileShare.Read);
 			var dataContractSerializer = new DataContractJsonSerializer(typeof(OAuthInfo));
 			var info = dataContractSerializer.ReadObject(stream) as OAuthInfo;
 			return info;
@@ -115,13 +195,84 @@ namespace TaskWorkspace.Backup.BackupHelpers
 				{
 					isolatedStorageFile.DeleteFile(DefaultFolderInfo);
 				}
-				var stream = isolatedStorageFile.OpenFile(DefaultFolderInfo, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None);
-				var dataContractSerializer = new DataContractJsonSerializer(typeof(Folder));
-				dataContractSerializer.WriteObject(stream, new Folder() { Id = folderId });
+				using (var stream = isolatedStorageFile.OpenFile(DefaultFolderInfo,FileMode.OpenOrCreate,FileAccess.ReadWrite,FileShare.None))
+				{
+					var dataContractSerializer = new DataContractJsonSerializer(typeof(Folder));
+					dataContractSerializer.WriteObject(stream,new Folder() { Id = folderId });
+				}
 			}
 		}
 
+		private string CreateAndGetDefaultFolderId (DriveService service)
+		{
+			var filelist = retrieveAllFiles(service,"mimeType = 'application/vnd.google-apps.folder' and name = 'TaskWorkspace'");
+			string folderId = filelist.Any() ? filelist.First().Id : CreateAndGetDefaultFolderId(service);
 
+			SaveDefaultFolderInfo(folderId);
+
+			return folderId;
+		}
+
+
+		private static string CreateFolderAndReturnId ( DriveService service,string name )
+		{
+			var fileMetadata = new File();
+			fileMetadata.Name = "TaskWorkspace";
+			fileMetadata.MimeType = "application/vnd.google-apps.folder";
+
+			var file = service.Files.Create(fileMetadata).Execute();
+			return file.Id;
+		}
+
+		public static List<File> retrieveAllFiles ( DriveService service,string searchQuery )
+		{
+			var result = new List<File>();
+			var request = service.Files.List();
+			//request.Fields = "id, webContentLink";
+			request.Q = searchQuery;
+
+			do
+			{
+				try
+				{
+					var files = request.Execute();
+
+					result.AddRange(files.Files);
+					request.PageToken = files.NextPageToken;
+				}
+				catch(Exception e)
+				{
+					WorkspaceLogger.Log.Error(e);
+					request.PageToken = null;
+				}
+			} while(!string.IsNullOrEmpty(request.PageToken));
+
+			return result;
+		}
+
+		private void UploadFile ( DriveService service,string fullFilename, string filename,string parentFolderId )
+		{
+			var removeLIst = retrieveAllFiles(service,$"name = '{filename}'");
+			foreach(var fileRemove in removeLIst)
+			{
+				service.Files.Delete(fileRemove.Id).Execute();
+			}
+
+			// upload
+			var fileMetadata = new File()
+			{
+				Name = filename,
+				Parents = new List<string>() { parentFolderId }
+			};
+			FilesResource.CreateMediaUpload request;
+			using(var stream = new System.IO.FileStream(fullFilename,
+									System.IO.FileMode.Open))
+			{
+				request = service.Files.Create(
+					fileMetadata,stream,DefaultMimeType);
+				request.Upload();
+			}
+		}
 
 	}
 }
