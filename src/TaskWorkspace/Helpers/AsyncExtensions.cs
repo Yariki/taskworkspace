6ea79eeebbebdc6e8659d5adbffd1c49112bbcc7
Yaroslav Lisovskyi <yaroslav.lisovskyi@evry.com>
2020-12-03 02:00:41 +0200

    fixed wrong usage og async/awawit pattern and added sync over async extenstions

---
+++ src/TaskWorkspace/Helpers/AsyncExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskWorkspace.Helpers
+{
+	internal static class AsyncExtensions
+	{
+
+		public static string ReadLine(this LinesReader reader, int line)
+		{
+			return RunSync(() => reader.ReadLineAsync(line));
+		}
+
+
+		private static T RunSync<T>(Func<Task<T>> doFunc)
+		{
+			return Task.Run(doFunc).Result;
+		}
+
+	}
+}
